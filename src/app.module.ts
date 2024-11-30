b098613fc0a2ad96eb4a590f594be3304bab561b
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-11-29 19:46:04 -0500

    Update .gitignore

---
+++ src/app.module.ts
@@ -0,0 +1,49 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {  ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModuleCustom } from './config/config.module';
+import { UsersModule } from './modules/users/users.module';
+import { join } from 'path';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './common/filters/general-exceptions.filter';
+
+import { AuthModule } from './auth/auth.module';
+import { IncomeModule } from './modules/income/income.module';
+import { ExpensesModule } from './modules/expenses/expenses.module';
+import { BudgetModule } from './modules/budget/budget.module';
+
+
+@Module({
+  imports: [
+  ConfigModuleCustom,
+  TypeOrmModule.forRoot({ 
+    type:'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [join(__dirname + '/**/*.entity{.ts,.js}')],
+    synchronize: true,}),
+  UsersModule,
+  AuthModule,
+
+  IncomeModule,
+  ExpensesModule,
+  BudgetModule
+  
+],
+  controllers: [AppController],
+  providers: [AppService,
+    ConfigService,
+    {
+      provide:APP_FILTER,
+      useClass:AllExceptionsFilter
+    }
+  
+  
+  ],
+})
+export class AppModule {}
